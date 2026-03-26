5e20348a7d2948bbb4595ccdfb07a74a83f27462
ElifSena204 <elif.gumusoglu@venta.lv>
2026-03-26 12:14:24 +0200

    created Vechicle class

---
+++ src/model/Vehicle.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Vehicle {
+	private int id;
+	private String title;
+	private String vehicleCode;
+	private float price;
+	private EnergyType etype;
+	
+	private static int counter = 0;
+	
+	public int getId() {
+		return id;
+	
+	}
+	
+	
+
+}
